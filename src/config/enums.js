9c5eaa84f311027fa0a0abb08608a6a7e29b171c
Maicom <msbernardi2@ucs.br>
2018-10-27 11:15:14 -0300

    Criado enum

--- src/config/enums.js
+++ src/config/enums.js
@@ -2,3 +2,8 @@ export const HIRING_TYPES = {
     REGULAR: { value: 'REGULAR', name: 'Efetivo' },
     INTERN: { value: 'INTERN', name: 'Estágio' }
 }
+
+export const HIRING_TYPES2 = {
+    REGULAR: { value: 'REGULAR', name: 'Efetivooo' },
+    INTERN: { value: 'INTERN', name: 'Estágiooo' }
+}
